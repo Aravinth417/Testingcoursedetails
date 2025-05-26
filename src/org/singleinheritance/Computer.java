aaf065264b8a521f086799fefe7bc5e519279191
Aravinth417 <aravinth15042002@gmail.com>
2025-05-26 10:35:24 +0530

    code accepted

---
+++ src/org/singleinheritance/Computer.java
@@ -0,0 +1,13 @@
+package org.singleinheritance;
+
+//single inheritance parent class
+
+public class Computer {
+	
+	public void computermodel() {
+		
+		System.out.println("computerseries");
+		
+	}
+
+}
---
+++ src/org/singleinheritance/Desktop.java
@@ -0,0 +1,22 @@
+package org.singleinheritance;
+
+//single inheritance parent class
+
+public class Desktop extends Computer {
+	
+	private void desktopsize() {
+		
+		System.out.println("desktopsbackground");
+		
+		
+	}
+	  public static void main(String[] args) {
+		  
+		  Desktop d = new Desktop();
+		  
+		  d.desktopsize();
+		  d.computermodel();
+		
+	}
+
+}
